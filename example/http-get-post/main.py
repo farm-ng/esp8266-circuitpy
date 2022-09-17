b3a31b29180da470e7ab955e900e76692a179412
Hackerman342 <kyle@farm-ng.com>
2022-09-17 17:47:25 -0400

    Download png in example - not formatted correctly

--- example/http-get-post/main.py
+++ example/http-get-post/main.py
@@ -61,38 +61,73 @@ while 1:
 print("\r\n\r\n")
 print("Now it's time to start HTTP Get/Post Operation.......\r\n")
 
-while 1:
-    led.toggle()
-    time.sleep(1)
+# while(1):
 
-    """
-    Going to do HTTP Get Operation with www.httpbin.org/ip, It return the IP address of the connected device
-    """
-    httpCode, httpRes = esp01.doHttpGet(
-        "www.httpbin.org", "/ip", "RaspberryPi-Pico", port=80
-    )
-    print(
-        "------------- www.httpbin.org/ip Get Operation Result -----------------------"
-    )
-    print("HTTP Code:", httpCode)
-    print("HTTP Response:", httpRes)
-    print(
-        "-----------------------------------------------------------------------------\r\n\r\n"
-    )
+# led.toggle()
+time.sleep(1)
 
-    """
-    Going to do HTTP Post Operation with www.httpbin.org/post
-    """
-    post_json = '{"name":"Noyel"}'
-    httpCode, httpRes = esp01.doHttpPost(
-        "www.httpbin.org", "/post", "RPi-Pico", "application/json", post_json, port=80
-    )
-    print(
-        "------------- www.httpbin.org/post Post Operation Result -----------------------"
-    )
-    print("HTTP Code:", httpCode)
-    print("HTTP Response:", httpRes)
-    print(
-        "--------------------------------------------------------------------------------\r\n\r\n"
-    )
-    # break
+"""
+Going to do HTTP Get Operation with www.httpbin.org/ip, It return the IP address of the connected device
+"""
+# httpCode, httpRes = esp01.doHttpGet("www.httpbin.org","/ip","Amiga-dashboard", port=80)
+# httpCode, httpRes = esp01.doHttpGet("api.thingspeak.com","/channels/1417/feeds.json","Amiga-dashboard", port=80)
+# httpCode, httpRes = esp01.doHttpGet("api.thingspeak.com","/channels/1417/feeds.json?results=1","Amiga-dashboard", port=80)
+httpCode, httpRes = esp01.doHttpGet(
+    "www.google.com",
+    "/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png",
+    "Amiga-dashboard",
+    port=80,
+)
+# httpCode, httpRes = esp01.doHttpGet("www.github.com", "/farm-ng/amiga-dev-kit/releases/download/amiga-dash-v0.0.3/amiga-dash-v0.0.3.zip","Amiga-dashboard", port=80)
+# httpCode, httpRes = esp01.doHttpGet("www.github.com","/farm-ng/amiga-dev-kit/raw/main/website/static/img/farm-ng_favicon.png","Amiga-dashboard", port=80)
+# httpCode, httpRes = esp01.doHttpGet("raw.githubusercontent.com","/farm-ng/amiga-dev-kit/main/website/static/img/farm-ng_favicon.png","Amiga-dashboard", port=80)
+print("------------- www.httpbin.org/ip Get Operation Result -----------------------")
+print("HTTP Code:", httpCode)
+# print("HTTP Response:",httpRes)
+print(
+    "-----------------------------------------------------------------------------\r\n\r\n"
+)
+
+
+print(type(httpRes))
+
+if httpRes is not None:
+    print("Writing to disk")
+    with open("test.txt", "wb") as f:
+        f.write(httpRes)
+
+    print("Writing as image")
+    with open("test.png", "wb") as f:
+        f.write(httpRes)
+else:
+    print("No http response to write")
+
+# storage.remount("/", True)
+
+"""
+Going to do HTTP Post Operation with www.httpbin.org/post
+"""
+post_json = '{"name":"Noyel"}'
+httpCode, httpRes = esp01.doHttpPost(
+    "www.httpbin.org",
+    "/post",
+    "Amiga-dashboard",
+    "application/json",
+    post_json,
+    port=80,
+)
+print(
+    "------------- www.httpbin.org/post Post Operation Result -----------------------"
+)
+print("HTTP Code:", httpCode)
+print("HTTP Response:", httpRes)
+print(
+    "--------------------------------------------------------------------------------\r\n\r\n"
+)
+
+
+import os
+import microcontroller
+
+os.remove("/NO_USB")
+microcontroller.reset()
